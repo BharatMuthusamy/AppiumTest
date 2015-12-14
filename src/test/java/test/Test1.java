11a9f5626c43ebc66692d4cf56276cb94f5740d4
Bharat <login2bharath@gmail.com>
2015-12-14 19:25:44 +0000

    Changed output message

--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -11,9 +11,7 @@ public class Test1 {
 
     @Test
     public void test(){
-        WebDriver driver = new FirefoxDriver();
-        driver.get("http://beta.prospects.co.uk");
-        System.out.println(driver.getPageSource());
+        System.out.println("Console output message");
     }
 
 }
