6c30408cd02a87880c9f2171e1dda5fbe666750f
Bharat <login2bharath@gmail.com>
2015-12-23 15:14:50 +0000

    Created driver helper

---
+++ src/test/java/test/Test2.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Bharat on 14/12/2015.
+ */
+public class Test2 {
+    @Test
+    public void test2(){
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
